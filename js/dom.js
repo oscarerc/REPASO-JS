ad699a343a024c48583593e5311a4c9b2edab3c9
oscarerc <66396097+oscarerc@users.noreply.github.com>
2023-02-18 23:34:39 -0500

    hamburguesa menu

--- js/dom.js
+++ js/dom.js
@@ -268,6 +268,21 @@ window.addEventListener("scroll",(e)=>{
 
 const $btnAbrir= document.getElementById(".abrir-ventana"),
 $btnCerrar= document.getElementById(".cerrar-ventana"),
-$btnImprimir= document.getElementById(".imprimir-ventana")
+$btnImprimir= document.getElementById(".imprimir-ventana");
+
+
+let ventana;
+
+$btnAbrir.addEventListener("click",(e)=>{
+  ventana = window.open("https://facebook.com");
+});
+
+$btnCerrar.addEventListener("click",(e)=>{
+  ventana.close();
+});
+
+$btnImprimir.addEventListener("click",(e)=>{
+  ventana,print();
+});
 
 
