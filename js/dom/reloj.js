b995c23b87bb4c25b8ac3b51297ae247b085f5fd
oscarerc <66396097+oscarerc@users.noreply.github.com>
2023-03-05 12:46:35 -0500

    hamburguesa menu9

---
+++ js/dom/reloj.js
@@ -0,0 +1,13 @@
+const d=document;
+
+export function digitalClock(clock,btnPlay,btnStop){
+d.addEventListener("click",e=>{
+    if(e.target.matches(btnPlay)){
+
+    }
+})
+}
+
+export function alarm(){
+    
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,8 +1,11 @@
 
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
+// uando no hay default se realiza de la siguiente forma
+import { digitalClock,alarm } from "./dom/reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded",(e)=>{
     hamburgerMenu(".panel-btn",".panel", ".menu a");
 })
+ 
