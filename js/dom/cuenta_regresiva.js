4da6b4e84e0a6a83e4816b0374d70c2520877690
oscarerc <66396097+oscarerc@users.noreply.github.com>
2023-03-08 00:01:32 -0500

    hamburguesa menu cuenta_regresiva1

--- js/dom/cuenta_regresiva.js
+++ js/dom/cuenta_regresiva.js
@@ -7,10 +7,19 @@ countdownDate= new Date(limitDate).getTime();
 let countdownTempo =setInterval(()=>{
     let now= new Date().getTime(),
     limitTime=countdownDate-now,
-    days,
-    hours,
-    minutes,
-    seconds;
-    console.log(countdownDate);
-},1000)
+    days=Math.floor(limitTime/(1000*60*60*24)),
+    hours=("0"+Math.floor(limitTime%(1000*60*60*24)/(1000*60*60))).slice(-2),
+    minutes=("0"+Math.floor(limitTime%(1000*60*60)/(1000*60))).slice(-2),
+    seconds=("0"+Math.floor(limitTime%(1000*60)/(1000))).slice(-2);
+
+
+    $countdown.innerHTML =`<h3>Faltan: ${days} dias ${hours} horas ${minutes} minutos ${seconds} segundos</h3>`;
+    // console.log(countdownDate);
+
+    if(limitTime<0){
+        clearInterval(countdownTempo);
+        $countdown.innerHTML =`<h3> ${finalMessage} </h3>`;
+    }
+
+},1000);
 }
