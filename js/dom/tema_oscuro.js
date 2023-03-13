c05ea8a6ff8c460728f5b08d3d94b75ff4522581
oscarerc <66396097+oscarerc@users.noreply.github.com>
2023-03-12 22:39:24 -0500

    hamburguesa menu dark theme2

--- js/dom/tema_oscuro.js
+++ js/dom/tema_oscuro.js
@@ -3,7 +3,27 @@ const d= document;
 export default function darkTheme(btn, classDark){
     const $themeBtn =d.querySelector(btn),
     // en css el uso de [] hace referencia a la lista del mismo atributo
-    $selectors=d.querySelectorAll("[dat-dark]");
+    $selectors=d.querySelectorAll("[data-dark]");
 
     console.log($selectors); 
+
+    let moon="🌙",
+    sun="☀️";
+
+    d.addEventListener("click", (e)=>{
+        if(e.target.matches(btn)){
+            console.log($themeBtn.textContent);
+            if($themeBtn.textContent===moon){
+                $selectors.forEach((el)=>{
+                    el.classList.add(classDark);
+                    $themeBtn.textContent=sun;
+                })
+            }else{
+                $selectors.forEach((el)=>{
+                    el.classList.remove(classDark);
+                    $themeBtn.textContent=moon;
+                })
+            }
+        }
+    })
 }
