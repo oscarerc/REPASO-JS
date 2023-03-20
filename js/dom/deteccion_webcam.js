d2733a63f661cacd39b7634bd6751a829935f71f
oscarerc <66396097+oscarerc@users.noreply.github.com>
2023-03-19 22:23:51 -0500

    videeo2

--- js/dom/deteccion_webcam.js
+++ js/dom/deteccion_webcam.js
@@ -1,11 +1,20 @@
+const d = document,
+  n = navigator;
 
-const d= document,
-n= navigator;
+export default function webcam(id) {
+  const $video = d.getElementById(id);
 
-export default function webcam(id){
-    const $video = d.getElementById(id);
-
-    if(n.mediaDevices.getUserMedia){
-        n.mediaDevices.getUserMedia({video:true, audio:true})
-    }
+  if (n.mediaDevices.getUserMedia) {
+    n.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        console.log(stream);
+        $video.srcObject= stream;
+        $video.play();
+      })
+      .catch((err) => {
+        $video.insertAdjacentElement("afterend",`<p><mark>${err}</mark></p>`)
+        console.log(err);
+      });
+  }
 }
