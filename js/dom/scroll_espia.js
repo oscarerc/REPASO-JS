b3c7845df71d607a02d010053c8d7039fc2cf059
oscarerc <66396097+oscarerc@users.noreply.github.com>
2023-03-25 17:33:39 -0500

    scroll3

--- js/dom/scroll_espia.js
+++ js/dom/scroll_espia.js
@@ -1,5 +1,32 @@
 const d= document;
 
 export default function scrollSpy(){
-    
+    const sections = d.querySelectorAll("section[data-scroll-spy]");
+    const cb =(entries)=>{
+        //console.log("entries", entries);
+
+        entries.forEach((entry)=>{
+            // cada entry
+            // console.log("entry", entry);
+
+            const id= entry.target.getAttribute("id");
+            console.log(id);
+
+            if(entry.isintersecting){
+                d.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.add("active");
+            }else{
+                d.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.remove("active");
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(cb,{
+        rootMargin:"-250px",
+        // elementos porcentaje de vista
+        // threshold:0.5
+    });
+
+    $sections.array.forEach(el => {
+        observer.observe(el);
+    });
 }
